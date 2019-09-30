7ad0278c22987e9b34ea64aa774e05675530462a
Brandon Teague <brandon.teague13@icloud.com>
2019-09-30 08:27:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,36 @@ function nowServing(katzDeli) {
   else
   return (`Currently serving ${katzDeli.shift()}.`);
 }
+// Build a function currentLine that returns the current line.
+// For example, if katzDeliLine is currently ["Ada", "Grace"],
+// currentLine(katzDeliLine) would return "The line is currently: 1. Ada 2. Grace".
+// If there is nobody in line, it should return "The line is currently empty."
+
+
+var line = [];
+
+
+
+
+
+function currentLine(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
+
+//WORKS:::::
+
+// function currentLine(katzDeli) {
+//
+//   for (let i = 0; i < katzDeli.length; i++) {
+//     line.push(` `+[i+1]+`. `  + katzDeli[i])
+//   }
+//   return(`The line is currently:` + line);
+// }
